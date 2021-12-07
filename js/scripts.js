2bb747a6f528eccf0584e1bb927d1674968ad655
domwitt21 <60725257+domwitt21@users.noreply.github.com>
2021-12-07 16:59:03 -0500

    Updated JS file
    Declared a new array pokekmonList() with several objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world!');
+let pokemonList = [
+    {name: 'Charizard', height: 1.7, weight: 90.5, type: ['Fire', 'Flying']},
+    {name: 'Tyranitar', height: 2, weight: 202, type: ['Dark', 'Rock']},
+    {name: 'Gengar', height: 1.5, weight: 40.5, type: ['Ghost', 'Poison']}
+];
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+console.log(pokemonList);
