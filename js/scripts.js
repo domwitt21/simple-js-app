6ed9462a1bde2f72e3b2ed8628a2b0719221dabb
domwitt21 <60725257+domwitt21@users.noreply.github.com>
2021-12-08 23:16:56 -0500

    Added a for loop
    Added a for loop to iterate over our pokemon array objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
+// Declaring a variable and assigning it an array containing pokemon objects
 let pokemonList = [
     {name: 'Charizard', height: 1.7, weight: 90.5, type: ['Fire', 'Flying']},
     {name: 'Tyranitar', height: 2, weight: 202, type: ['Dark', 'Rock']},
     {name: 'Gengar', height: 1.5, weight: 40.5, type: ['Ghost', 'Poison']}
 ];
-
+// Logging the array to the console to test code
 console.log(pokemonList);
+
+// for loop iterating over the pokemonList array objects
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 2) {
+        document.write('<h5> Name: </h5>' + pokemonList[i].name + '<h5> Height: </h5>' + pokemonList[i].height + ' \-\- Wow. that\'s big!');
+    } else {
+        document.write('<h5> Name: </h5>' + pokemonList[i].name + '<h5> Height: </h5>' + pokemonList[i].height);
+    }
+}
